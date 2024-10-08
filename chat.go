c85b1441981bb6e70770384a351f0bc085bb3033
sanjay920 <sanjay.nadhavajhala@gmail.com>
2024-10-08 09:26:43 -0700

    add store field

--- chat.go
+++ chat.go
@@ -194,6 +194,7 @@ type ChatCompletionRequest struct {
 	Temperature      *float32                      `json:"temperature,omitempty"`
 	TopP             float32                       `json:"top_p,omitempty"`
 	N                int                           `json:"n,omitempty"`
+	Store            *bool                         `json:"store,omitempty"`
 	Stream           bool                          `json:"stream,omitempty"`
 	StreamOptions    *StreamOptions                `json:"stream_options,omitempty"`
 	Stop             []string                      `json:"stop,omitempty"`
